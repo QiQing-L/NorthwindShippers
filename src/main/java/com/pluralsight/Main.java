ab0ffdcd01e2cdb0a95ff84db65093c27c52cb43
Qi Qing <learn.qiqing@gmail.com>
2025-12-02 13:58:02 -0500

    created classes, added arguments for local database northwind in Main. added dependencies to pom.xml. model shipper class completed with attributes, constructor , getters and toString(). db DataManager class added attribute and constructor no methods yet.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class Main {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("User and Password are needed!");
+            System.exit(1);
+        }
+
+        String username = args[0];
+        String password = args[1];
+
+        BasicDataSource northwindDataSource = new BasicDataSource();
+
+    }
 }
--- src/main/java/com/pluralsight/model/Shipper.java
+++ src/main/java/com/pluralsight/model/Shipper.java
@@ -1,4 +1,32 @@
 package com.pluralsight.model;
 
 public class Shipper {
+    private int shipperID;
+    private String companyName;
+    private String Phone;
+
+    public Shipper(int shipperID, String companyName, String phone) {
+        this.shipperID = shipperID;
+        this.companyName = companyName;
+        Phone = phone;
+    }
+
+    public int getShipperID() {
+        return shipperID;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    @Override
+    public String toString() {
+        return "shipper ID:" + shipperID +
+                ", companyName:'" + companyName + '\'' +
+                ", Phone:'" + Phone + '\'';
+    }
 }
